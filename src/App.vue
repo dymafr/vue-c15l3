4aceee0ecd113695f6ec5c6a60f31dc45e9ba93f
dymafr <erwan1309@hotmail.com>
2022-03-26 11:46:11 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,15 @@
   <input v-focus type="text" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Directive } from 'vue';
+
+const vFocus: Directive<HTMLInputElement, boolean> = {
+  mounted(el, binding) {
+    el.focus();
+  },
+};
+</script>
 
 <style lang="scss">
 @import './assets/scss/base.scss';
