dc12f96a114016b39373e8c2ad5c9f5c4cf87762
dymafr <erwan1309@hotmail.com>
2022-03-26 11:48:19 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <input v-focus type="text" />
+  <input v-focus="{ firstFocus: true, color: 'green' }" type="text" />
 </template>
 
 <script setup lang="ts">
 import type { Directive } from 'vue';
 
-const vFocus: Directive<HTMLInputElement, boolean> = {
+const vFocus: Directive<
+  HTMLInputElement,
+  { firstFocus: boolean; color: string }
+> = {
   mounted(el, binding) {
     el.focus();
   },
